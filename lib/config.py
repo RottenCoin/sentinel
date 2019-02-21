f15a71da1ba49ad6c473d34773de0a4188853848
RottenCoin <38838284+RottenCoin@users.noreply.github.com>
2019-02-21 20:55:36 +0000

    Add files via upload

--- lib/config.py
+++ lib/config.py
@@ -3,30 +3,30 @@
 """
 import sys
 import os
-from dash_config import DashConfig
+from omegacoin_config import OmegaCoinConfig
 
 default_sentinel_config = os.path.normpath(
     os.path.join(os.path.dirname(__file__), '../sentinel.conf')
 )
 sentinel_config_file = os.environ.get('SENTINEL_CONFIG', default_sentinel_config)
-sentinel_cfg = DashConfig.tokenize(sentinel_config_file)
-sentinel_version = "1.3.0"
-min_dashd_proto_version_with_sentinel_ping = 70207
+sentinel_cfg = OmegaCoinConfig.tokenize(sentinel_config_file)
+sentinel_version = "1.2.0"
+min_omegacoind_proto_version_with_sentinel_ping = 70207
 
 
-def get_dash_conf():
+def get_omegacoin_conf():
     if sys.platform == 'win32':
-        dash_conf = os.path.join(os.getenv('APPDATA'), "DashCore/dash.conf")
+        omegacoin_conf = os.path.join(os.getenv('APPDATA'), "OmegaCoinCore/omegacoin.conf")
     else:
         home = os.environ.get('HOME')
 
-        dash_conf = os.path.join(home, ".dashcore/dash.conf")
+        omegacoin_conf = os.path.join(home, ".omegacoincore/omegacoin.conf")
         if sys.platform == 'darwin':
-            dash_conf = os.path.join(home, "Library/Application Support/DashCore/dash.conf")
+            omegacoin_conf = os.path.join(home, "Library/Application Support/OmegaCoinCore/omegacoin.conf")
 
-    dash_conf = sentinel_cfg.get('dash_conf', dash_conf)
+    omegacoin_conf = sentinel_cfg.get('omegacoin_conf', omegacoin_conf)
 
-    return dash_conf
+    return omegacoin_conf
 
 
 def get_network():
@@ -86,7 +86,7 @@ def get_db_conn():
     return db
 
 
-dash_conf = get_dash_conf()
+omegacoin_conf = get_omegacoin_conf()
 network = get_network()
 rpc_host = get_rpchost()
 db = get_db_conn()
